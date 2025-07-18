be51de6384255f87f3c1b1a5f78e580b471e0bb5
Fekri Saleh <fekri600@gmail.com>
2025-07-17 23:41:30 -0600

    update provider

--- app_deploy/providers.tf
+++ app_deploy/providers.tf
@@ -21,9 +21,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_eks_cluster" "this" {
-  name = var.cluster_name
-}
+
 
 data "aws_eks_cluster_auth" "this" {
   name = var.cluster_name
@@ -31,8 +29,8 @@ data "aws_eks_cluster_auth" "this" {
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+    host                   = var.cluster_endpoint
+    cluster_ca_certificate = base64decode(var.cluster_ca_certificate_data)
     token                  = data.aws_eks_cluster_auth.this.token
   }
 }
